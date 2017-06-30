6c3d49bcde482b4ef635b9611ee9b7189414c614
Hayoung Yun <hayoungyun@hayoungs-mbp.1stavemachines.local>
2017-06-30 11:36:37 -0400

    first commit

---
+++ blog.php
@@ -0,0 +1,27 @@
+<?php
+<head> 
+	<meta charset="UTF-8" /> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Special Guest Book</title>
+	<link rel="stylesheet" href="styles.css" type="text/css" />
+	<link rel="icon" type="image/png" href="">
+</head>
+
+<body>
+
+	// <div class="container">
+	// 	<img class="logo" src="logo.png"/>
+	// </div>
+
+	<div class="entry-container">
+		<div class="entry">
+			<h1 class="title"></h1>
+			<h3 class="date"></h3><img class="entry-image" src=""/>
+			<div class="post"></div>
+		</div>
+	
+	</div>
+
+</body>
+
+?>
---
+++ main.js
@@ -0,0 +1,47 @@
+
+function myFunction() {
+    var x = document.getElementById('more_text');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
+// function slide() {
+
+// }
+
+
+// $( ".read_more" ).click(function() {
+// 	document.getElementsByClassName('more_text').style.display = 'block';
+// 	$( ".more_text" ).slideDown(600);
+// });
+
+
+// $( document.body ).click(function () {
+
+// });
+
+
+
+/////// Neil's code /////////
+
+// var initReadMoreButtons = function() {
+
+// var readMoreButtons = document.getElementsByClassName( 'read_more' );
+
+// 	for( var i = 0; i < readMoreButtons.length; i++ ) {
+
+//  	var thisButton = readMoreButtons[ i ];
+// 		thisButton.onclick = function( event ) {
+
+// 			event.preventDefault();
+
+// 		}
+
+// 	}
+
+// }
